74afc1b68a9d7fb62ad12fe6d506842678e1e915
gyj <gyj@gmail.com>
2025-06-02 09:04:02 +0900

    로그인 저장 시 localstorage에 저장 로직 추가

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -6,8 +6,16 @@ const api = axios.create({
 })
 
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('accessToken')
+    const token = localStorage.getItem('accessToken');
+
+    //Content-Type 자동 설정
+    const isFormData = config.data instanceof FormData
+    if (!isFormData) {
+        config.headers['Content-Type'] = 'application/json'
+    }
+    // Authorization 헤더 추가
     if (token) config.headers.Authorization = `Bearer ${token}`
+    
     return config
 })
 
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -9,7 +9,10 @@ const AuthService = {
   login: async (dispatch: AppDispatch,  data: LoginRequest) => {
     console.log(TAG, 'data >> ', data);
     const res = await AuthApi.postLogin(data);
+    // Redux 저장
     dispatch(setLogin(res));
+    // LocalStorage 저장
+    localStorage.setItem('accessToken', res.accessToken);
   },
 };
 
