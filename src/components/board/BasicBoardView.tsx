26e010d1a9a67279d1da9413e84d0ab77e991893
gyj <gyj@gmail.com>
2025-06-23 11:37:57 +0900

    redux는 새로고침 시 초기화됨. localstorage에 저장된 걸 자동으로 가져와서 저장해주는 방법 추가

---
+++ src/components/board/BasicBoardView.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BasicBoardView = () => {
+  return (
+    <div>
+      <h3>기본 게시판 상세 조회</h3>
+    </div>
+  );
+};
+
+export default BasicBoardView;
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
-import React from 'react'
-import { Provider } from 'react-redux'
-import store from './store/store'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import React from 'react';
+
+import { Provider } from 'react-redux';
+import store, { persistor } from './store/store'; // ✅ persistStore도 함께 import
+import { PersistGate } from 'redux-persist/integration/react'; // ✅ persist gate 컴포넌트 import
 
 createRoot(document.getElementById('root')!).render(
-    
-    <React.StrictMode>
-    <Provider store={store}>
-      <App />
+  <React.StrictMode>
+    <Provider store={store}> {/* Redux 상태 공급 */}
+      <PersistGate loading={null} persistor={persistor}> {/* ✅ 새로고침 시 localStorage에서 복구될 때까지 기다려줌 */}
+        <App /> {/* App이 복구 완료 후에 렌더링됨 */}
+      </PersistGate>
     </Provider>
   </React.StrictMode>
-
-)
+);
